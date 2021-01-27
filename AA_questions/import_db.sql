eb0a27f29cb5413a16b39891432c2caabe954197
alejandro <alejandroweil13@gmail.com>
2021-01-27 14:06:29 -0500

    Finished 4 tables and inserted data into tables users and questions

--- AA_questions/import_db.sql
+++ AA_questions/import_db.sql
@@ -9,10 +9,10 @@ CREATE TABLE users(
 CREATE TABLE questions(
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
-    body TEXT NOT,
+    body TEXT NOT NULL,
     associated_author_id INTEGER NOT NULL,
 
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    FOREIGN KEY(associated_author_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows(
@@ -36,11 +36,27 @@ CREATE TABLE replies(
 
  CREATE TABLE question_likes(
      id serial PRIMARY KEY,
+     thumbs_up? BOOLEAN,
+     user_id INTEGER,
+     question_id INTEGER,
 
- )
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(question_id) REFERENCES questions(id) ON DELETE CASCADE     
+ );
 
 
+-- This is where we seed the database.
+INSERT INTO
+    users (fname, lname)
+  VALUES
+    ('Elon', 'Musk'),
+    ('Jeff', 'Bezos');
 
+INSERT INTO
+    questions (title, body, associated_author_id)
+  VALUES
+    ('Car has no Battery', 'Where is the closest supercharger?', SELECT id FROM users WHERE fname = 'Elon'),
+    ('Divorce lawyers near me', 'Any decent lawyers around DC?', SELECT id FROM users WHERE lname = 'Bezos');
 
 
 
