f9cb7eff425b72f057118928d338ddb5c50236da
rafael silva <silvaserranor@gmail.com>
2021-01-27 14:43:50 -0500

    added dropped tables to begining of file

--- AA_questions/import_db.sql
+++ AA_questions/import_db.sql
@@ -1,10 +1,17 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS question_likes; 
+DROP TABLE IF EXISTS replies; 
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users; 
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL
 );
+
 
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
@@ -15,6 +22,7 @@ CREATE TABLE questions (
     FOREIGN KEY (associated_author_id) REFERENCES users(id)
 );
 
+ 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
@@ -34,6 +42,7 @@ CREATE TABLE replies (
     FOREIGN KEY (parent_id) REFERENCES replies(id) ON DELETE CASCADE
  );
 
+ 
  CREATE TABLE question_likes (
      id INTEGER PRIMARY KEY,
      thumbs_up BOOLEAN,
