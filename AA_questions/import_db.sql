7be4ddf5a87e390ef97583730d9b66c40008fb51
rafael silva <silvaserranor@gmail.com>
2021-01-27 14:25:35 -0500

    Corrected syntax errors across the file

--- AA_questions/import_db.sql
+++ AA_questions/import_db.sql
@@ -1,47 +1,47 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE users(
-    id serial PRIMARY KEY,
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
+    lname TEXT NOT NULL
 );
 
-CREATE TABLE questions(
+CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
     associated_author_id INTEGER NOT NULL,
 
-    FOREIGN KEY(associated_author_id) REFERENCES users(id)
+    FOREIGN KEY (associated_author_id) REFERENCES users(id)
 );
 
-CREATE TABLE question_follows(
-    id serial PRIMARY KEY,
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY(question_id) REFERENCES questions(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
 );
 
-CREATE TABLE replies(
-    id serial PRIMARY KEY,
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
     body TEXT NOT NULL,
-    parent_id INTEGER -- Can be null if its the parent.  
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(question_id) REFERENCES questions(id) ON DELETE CASCADE,
-    FOREIGN KEY(parent_id) REFERENCES replies(id) ON DELETE CASCADE
+    parent_id INTEGER, -- Can be null if its the parent.  
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE,
+    FOREIGN KEY (parent_id) REFERENCES replies(id) ON DELETE CASCADE
  );
 
- CREATE TABLE question_likes(
-     id serial PRIMARY KEY,
-     thumbs_up? BOOLEAN,
+ CREATE TABLE question_likes (
+     id INTEGER PRIMARY KEY,
+     thumbs_up BOOLEAN,
      user_id INTEGER,
      question_id INTEGER,
 
-    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY(question_id) REFERENCES questions(id) ON DELETE CASCADE     
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE     
  );
 
 
@@ -55,8 +55,8 @@ INSERT INTO
 INSERT INTO
     questions (title, body, associated_author_id)
   VALUES
-    ('Car has no Battery', 'Where is the closest supercharger?', SELECT id FROM users WHERE fname = 'Elon'),
-    ('Divorce lawyers near me', 'Any decent lawyers around DC?', SELECT id FROM users WHERE lname = 'Bezos');
+    ('Car has no Battery', 'Where is the closest supercharger?', (SELECT id FROM users WHERE fname = 'Elon')),
+    ('Divorce lawyers near me', 'Any decent lawyers around DC?', (SELECT id FROM users WHERE lname = 'Bezos'));
 
 
 
