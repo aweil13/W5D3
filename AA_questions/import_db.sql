f9a3d9b0ef0bf55a734b117a8cfe7c72fa85e8f6
rafael silva <silvaserranor@gmail.com>
2021-01-27 13:33:24 -0500

    created linked tables

--- AA_questions/import_db.sql
+++ AA_questions/import_db.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE users (
+CREATE TABLE users(
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL,
@@ -12,10 +12,29 @@ CREATE TABLE questions(
     body TEXT NOT,
 
     FOREIGN KEY(user_id) REFERENCES users(id)
-)
+);
 
 CREATE TABLE question_follows(
+    id serial PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(question_id) REFERENCES questions(id) ON DELETE CASCADE
+);
 
-)
+CREATE TABLE replies(
+    id serial PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+    parent_id INTEGER -- Can be null if its the parent.  
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(question_id) REFERENCES questions(id) ON DELETE CASCADE,
+    FOREIGN KEY(parent_id) REFERENCES replies(id) ON DELETE CASCADE
+ );
+
+ 
+
+
 
 
